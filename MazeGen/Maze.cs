d557eae4800fca5072808da23cc4580b0507d0aa
catahoc <maxobimit@gmail.com>
2013-04-15 23:46:39 +0400

    Удалены лишине файлы. Добавлен конвертер лабиринта в массив байт и назад

--- MazeGen/Maze.cs
+++ MazeGen/Maze.cs
@@ -9,10 +9,20 @@
             _walls = walls;
         }
 
-        public Wall this[Point p]
+	    public Maze(int x, int y, int z)
+	    {
+		    _walls = new Wall[x,y,z];
+	    }
+
+	    public Wall this[Point p]
         {
             get { return _walls[p.X, p.Y, p.Z]; }
             set { _walls[p.X, p.Y, p.Z] = value; }
+        }
+	    public Wall this[int x, int y, int z]
+        {
+            get { return _walls[x, y, z]; }
+            set { _walls[x, y, z] = value; }
         }
 
         public int Width { get { return _walls.GetLength(0); } }
---
+++ Mazica.Domain/BinaryMazeConverter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using MazeGen;
+
+namespace Mazica.Domain
+{
+	public class BinaryMazeConverter
+	{
+		private const string FINGERPRINT = "maze-format-v0.1";
+
+		public Maze FromByteArray(byte[] data)
+		{
+			var offset = 0;
+			var width = BitConverter.ToInt32(data, offset);
+
+			offset += 4;
+			var height = BitConverter.ToInt32(data, offset);
+
+			offset += 4;
+			var depth = BitConverter.ToInt32(data, offset);
+
+			offset += 4;
+			var maze = new Maze(width, height, depth);
+			for(var x = 0; x < width; x++)
+			{
+				for(var y = 0; y < height; y++)
+				{
+					for(var z = 0; z < depth; z++)
+					{
+						maze[x, y, z] = (Wall)data[offset++];
+					}
+				}
+			}
+			
+			return maze;
+		}
+
+		public byte[] ToByteArray(Maze maze)
+		{
+			var destination = new byte[4 * 3 + maze.Depth * maze.Height * maze.Width];
+			Array.Copy(BitConverter.GetBytes(maze.Width), 0, destination, 0, 4);
+			Array.Copy(BitConverter.GetBytes(maze.Height), 0, destination, 4, 4);
+			Array.Copy(BitConverter.GetBytes(maze.Depth), 0, destination, 8, 4);
+			var i = 0;
+			for(var x = 0; x < maze.Width; x++)
+			{
+				for(var y = 0; y < maze.Height; y++)
+				{
+					for(var z = 0; z < maze.Depth; z++)
+					{
+						destination[i++] = (byte)maze[x, y, z];
+					}
+				}
+			}
+			return destination;
+		}
+	}
+}
