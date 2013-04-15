12d7b319875c9c496621364350ad8b763f32cd8f
catahoc <maxobimit@gmail.com>
2013-04-15 23:22:43 +0400

    Какие-то изменения, забыл

---
+++ Mazica.Tests/PathFinder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mazica.Domain;
+using NUnit.Framework;
+
+namespace Mazica.Tests
+{
+	[TestFixture]
+    public class PathFinder
+    {
+		const int size = 10;
+		public bool IsEnd(Position pos)
+		{
+			var items = new[] {pos.Lon, pos.Lat, pos.Hei};
+			return items.All(t => t == size - 1);
+		}
+
+		public void TestManyGeneratedAlgorithms()
+		{
+			var generator = new RecursiveMazeGenerator();
+			var maze = generator.Generate(size, size, size);
+			var flags = new и[size,size,size];
+			var position = new Position();
+			while (!IsEnd(position))
+			{
+				
+			}
+		}
+    }
+}
---
+++ Mazica.Tests/Position.cs
@@ -0,0 +1,16 @@
+﻿namespace Mazica.Tests
+{
+	public struct Position
+	{
+		public int Lon;
+		public int Lat;
+		public int Hei;
+
+		public Position(int lon, int lat, int hei)
+		{
+			Lon = lon;
+			Lat = lat;
+			Hei = hei;
+		}
+	}
+}
